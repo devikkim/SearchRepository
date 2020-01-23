6bddbe1c644f9b4caf7022bcf736ee9933235d27
InKwon James Kim <devikkim@gmail.com>
2020-01-23 15:18:39 +0900

    initialize

---
+++ SearchRepository/Pods/Target Support Files/Pods-SearchRepository/Pods-SearchRepository-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SearchRepository : NSObject
+@end
+@implementation PodsDummy_Pods_SearchRepository
+@end
---
+++ SearchRepository/SearchRepository/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchRepository
+//
+//  Created by InKwon James Kim on 2020/01/23.
+//  Copyright © 2020 InKwon Devik Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
